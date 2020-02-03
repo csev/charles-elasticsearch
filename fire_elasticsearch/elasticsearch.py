07026c1c598b01cf2cb35c4c60d888ad4077d495
Paul Severance <paul.severance@gmail.com>
2020-02-03 14:24:29 -0500

    allow for http methods other than GET

--- fire_elasticsearch/elasticsearch.py
+++ fire_elasticsearch/elasticsearch.py
@@ -1,9 +1,21 @@
+import os
+
 import aiohttp
 from sanic import Blueprint
-from sanic.response import json
+from sanic.response import json, text
 
 
 class Elasticsearch(object):
+
+    __host__ = os.getenv('FIRE_ELASTICSEARCH_URI', 'http://localhost:9200')
+
+    @classmethod
+    def set_host(cls, uri):
+        cls.__host__ = uri
+
+    @classmethod
+    def get_host(cls):
+        return cls.__host__
 
     @classmethod
     def resource(cls, *args, **kargs):
@@ -17,15 +29,22 @@ class Elasticsearch(object):
         async def handler(*args, **kargs):
             return await cls.handler(*args, **kargs)
 
-        @bp.route('/elasticsearch/<path:path>')
+        @bp.route('/elasticsearch/<index>', methods=[ 'OPTIONS', 'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' ])
+        async def handler(*args, **kargs):
+            return await cls.handler(*args, **kargs)
+
+        @bp.route('/elasticsearch/<index>/<path:path>', methods=[ 'OPTIONS',  'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' ])
         async def handler(*args, **kargs):
             return await cls.handler(*args, **kargs)
 
         return bp
 
     @classmethod
-    async def handler(cls, request, path=''):
-        uri = f'http://192.168.99.100:9200/{path}'
+    async def handler(cls, request, index='', path=''):
+        uri = f'{cls.__host__}/{index}/{path}'
         async with aiohttp.ClientSession() as session:
             async with session.request(request.method, uri) as response:
-                return json(await response.json())
+                try:
+                    return json(await response.json())
+                except:
+                    return text(await response.text())
