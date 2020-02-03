788cfc834637238aa6f129a6d5b37b9a88dba90a
Paul Severance <paul.severance@gmail.com>
2020-02-03 15:13:55 -0500

    use jwt authentication

--- fire_elasticsearch/elasticsearch.py
+++ fire_elasticsearch/elasticsearch.py
@@ -4,10 +4,14 @@ import aiohttp
 from sanic import Blueprint
 from sanic.response import json, text
 
+from fire_api import webtoken, scope
+
 
 class Elasticsearch(object):
 
     __host__ = os.getenv('FIRE_ELASTICSEARCH_URI', 'http://localhost:9200')
+
+    __methods__ = [ 'OPTIONS', 'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' ]
 
     @classmethod
     def set_host(cls, uri):
@@ -25,26 +29,32 @@ class Elasticsearch(object):
 
         bp = Blueprint(*args, **kargs)
 
-        @bp.route('/elasticsearch/')
+        @bp.route('/elasticsearch/', methods=cls.__methods__)
+        @webtoken
+        @scope({ 'elasticsearch.administrator': True })
         async def handler(*args, **kargs):
             return await cls.handler(*args, **kargs)
 
-        @bp.route('/elasticsearch/<index>', methods=[ 'OPTIONS', 'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' ])
+        @bp.route('/elasticsearch/<index>', methods=cls.__methods__)
+        @webtoken
+        @scope({ 'elasticsearch.administrator': True, 'elasticsearch.index': '$index' })
         async def handler(*args, **kargs):
             return await cls.handler(*args, **kargs)
 
-        @bp.route('/elasticsearch/<index>/<path:path>', methods=[ 'OPTIONS',  'HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE' ])
+        @bp.route('/elasticsearch/<index>/<path:path>', methods=cls.__methods__)
+        @webtoken
+        @scope({ 'elasticsearch.administrator': True, 'elasticsearch.index': '$index' })
         async def handler(*args, **kargs):
             return await cls.handler(*args, **kargs)
 
         return bp
 
     @classmethod
-    async def handler(cls, request, index='', path=''):
+    async def handler(cls, request, index='', path='', token=None):
         uri = f'{cls.__host__}/{index}/{path}'
         async with aiohttp.ClientSession() as session:
             async with session.request(request.method, uri) as response:
                 try:
                     return json(await response.json())
-                except:
+                except aiohttp.ContentTypeError:
                     return text(await response.text())
