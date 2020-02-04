f3c6538e3a6e4ffb8f82270061cedbf614d55c68
Paul Severance <paul.severance@gmail.com>
2020-02-04 10:14:00 -0500

    add access log

--- fire_elasticsearch/access.py
+++ fire_elasticsearch/access.py
@@ -6,19 +6,26 @@ from fire_api import TimestampMixin
 
 
 class Access(PostgresDBModel, TimestampMixin):
-    created = Field(type='timestamp', computed=lambda: datetime.now(), computed_empty=True)
-    accessed = Field(type='timestamp', computed=lambda: datetime.now())
+    created = Field(type='timestamp', computed=lambda: datetime.now(), computed_empty=True, computed_type=True)
+    accessed = Field(type='timestamp', computed=lambda: datetime.now(), computed_type=True)
     username = Field(required=True)
-    body = Field(type=dict, required=True)
+    request = Field(type=dict)
+    response = Field(type=dict)
 
 
 def access(handler):
     async def decorator(request, *args, **kargs):
-        index = kargs.get('index', 'administrator')
+        index = kargs.get('index')
+        if not index:
+            return await handler(request, *args, **kargs)
         model = await Access.find_one({ 'WHERE': f'data->>\'username\' = \'{index}\'' })
         if not model:
-            model = await Access.add({ 'username': index })
-        model.body = loads(request.body)
+            model = Access({ 'username': index })
+        if request.body:
+            model.request = loads(request.body)
+        response = await handler(request, *args, **kargs)
+        if response.body:
+            model.response = loads(response.body)
         await model.save()
-        return await handler(request, *args, **kargs)
+        return response
     return decorator
